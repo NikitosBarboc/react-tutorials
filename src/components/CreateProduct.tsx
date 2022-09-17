1b14921701212046a9089eb44cb829fe9518011c
Nik-portfolio <kravtcovmikita@gmail.com>
2022-09-17 09:48:49 +0300

    feat: add additional functionality

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -21,30 +21,51 @@ interface CreateProductProps {
 }
 
 function CreateProduct({ onCreate }: CreateProductProps) {
-  const [value, setValue] = useState('');
+  const [values, setValues] = useState({} as Iproduct);
   const [error, setError] = useState('');
   const submitHandler = async (e: React.FormEvent) => {
     setError('');
     e.preventDefault();
-    if (value.trim().length === 0) {
+    if (Object.keys(values).length === 0) {
       setError('Please enter valid title.');
       return;
     }
-    productData.title = value;
+    productData.title = values.title;
+    productData.price = values.price;
+    productData.description = values.description;
 
     const response = await axios.post<Iproduct>('https://fakestoreapi.com/products', productData);
     onCreate(response.data);
   };
   const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
+    const target = e.currentTarget;
+    const attribute = target.getAttribute('data-input') as keyof Iproduct;
+    setValues(Object.assign(values, { [attribute]: target.value }));
   };
   return (
     <form onSubmit={submitHandler}>
       <input
         type="text"
         className="border py-2 px-4 mb-2 w-full  outline-0"
+        data-input="title"
         placeholder="Enter product title..."
-        value={value}
+        value={values.title}
+        onChange={changeHandler}
+      />
+      <input
+        type="number"
+        className="border py-2 px-4 mb-2 w-full  outline-0"
+        data-input="price"
+        placeholder="Enter product price..."
+        value={values.price}
+        onChange={changeHandler}
+      />
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full  outline-0"
+        data-input="description"
+        placeholder="Enter product description..."
+        value={values.description}
         onChange={changeHandler}
       />
       {error && <Error error={error} />}
--- src/models.ts
+++ src/models.ts
@@ -9,4 +9,4 @@ export default interface Iproduct {
     rate: number,
     count: number
   }
-};
+}
